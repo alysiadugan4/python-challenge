ad2592f82f80f6ccbbe7531a051d76c74c1f2d78
Alysia Dugan <81889411+alysiadugan4@users.noreply.github.com>
2021-07-25 20:01:52 -0700

    adding base code to PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,32 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources','budget_data.csv')
+
+total_profit_losses = 0
+current = 0
+last = 0
+months = 0
+total_change = 0
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+
+    for row in csvreader:
+
+        total_profit_losses = total_profit_losses + int(row[1])
+
+        months = months + 1
+
+        current = int(row[1])
+
+        if months > 1:
+
+            change = current - last
+
+            total_change = total_change + change
+
+        last = int(row[1])
+
+        average_change = total_change/months
+
